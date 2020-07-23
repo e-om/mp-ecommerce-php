f88723e12737849fcdddbef5a664540ff0acc246
Sergio N <eommail@gmail.com>
2020-07-23 12:09:10 -0300

    Cambio en el check IPN

--- hook-ipn.php
+++ hook-ipn.php
@@ -8,26 +8,28 @@ MercadoPago\SDK::setAccessToken($mpConfig['user-mp-app']['access_token']);
 //$_POST["id"]='1615978805';
 if (isset($_POST["type"], $_POST["id"])) {
 
-    switch ($_POST["type"]) {
-        case "payment":
-            $data = MercadoPago\Payment::find_by_id($_POST["id"]);
-            break;
-        case "plan":
-            $data = MercadoPago\Plan::find_by_id($_POST["id"]);
-            break;
-        case "subscription":
-            $data = MercadoPago\Subscription::find_by_id($_POST["id"]);
-            break;
-        case "invoice":
-            $data = MercadoPago\Invoice::find_by_id($_POST["id"]);
-            break;
-    }
+//    switch ($_POST["type"]) {
+//        case "payment":
+//            $data = MercadoPago\Payment::find_by_id($_POST["id"]);
+//            break;
+//        case "plan":
+//            $data = MercadoPago\Plan::find_by_id($_POST["id"]);
+//            break;
+//        case "subscription":
+//            $data = MercadoPago\Subscription::find_by_id($_POST["id"]);
+//            break;
+//        case "invoice":
+//            $data = MercadoPago\Invoice::find_by_id($_POST["id"]);
+//            break;
+//    }
+
+    $data = controlIPN(json_encode($_POST), $mpConfig['user-mp-app']['access_token']);
 
     if (empty($data) === false) {
-//        $file_name = date('Y-m-d').'-ipn.txt';
+
         $file_name = 'ipn.txt';
         $fp = fopen($file_name, "a+");
-        fwrite($fp, json_encode($data)."\n");
+        fwrite($fp, json_encode($data)."\n\n");
         fclose($fp);
         header("HTTP/1.1 200 OK");
         exit(0);
@@ -64,16 +66,52 @@ if (isset($_GET["topic"], $_GET["id"])) {
     }
 
     if(empty($merchant_order)===false){
-//        $file_name = date('Y-m-d').'-ipn.txt';
         $file_name = 'ipn.txt';
         $fp = fopen($file_name, "a+");
-        fwrite($fp, json_encode($merchant_order)."\n");
+        fwrite($fp, json_encode($merchant_order)."\n\n");
         fclose($fp);
         header("HTTP/1.1 200 OK");
         exit(0);
     }
 }
 
-//}
+function controlIPN($obj_json_query_ipn = null, $key=null)
+{
+    if (empty($obj_json_query_ipn)) {
+        return false;
+    }
+
+    $obj_json_ipn = (object)json_decode($obj_json_query_ipn, true);
+
+    if (is_object($obj_json_ipn) === false) {
+        return false;
+    }
+
+    switch (true) {
+        case (isset($obj_json_ipn->id, $obj_json_ipn->topic) && $obj_json_ipn->topic === 'payment'):
+            $d = @file_get_contents('https://api.mercadopago.com/v1/payments/' . $obj_json_ipn->id . '?access_token=' . $key);
+            break;
+        case (isset($obj_json_ipn->data_id, $obj_json_ipn->type) && $obj_json_ipn->type === 'payment'):
+            $d = @file_get_contents('https://api.mercadopago.com/v1/payments/' . $obj_json_ipn->data_id . '?access_token=' . $key);
+            break;
+        case (isset($obj_json_ipn->collection_id)):
+            $d = @file_get_contents('https://api.mercadopago.com/v1/payments/' . $obj_json_ipn->collection_id . '?access_token=' . $key);
+            break;
+        default:
+            return null;
+            break;
+    }
+
+    if (empty($d) === false) {
+
+        $obj_mp = (object)json_decode($d, true);
+
+        if (isset($obj_mp->status)) {
+            return $obj_mp;
+        }
+    }
+    return null;
+}
+
 $txt = file_get_contents('ipn.txt');
 echo $txt;
